c7792ca4ca86cd5f37e21197555b30820d5d98fd
ayong <940606334@qq.com>
2018-03-18 15:27:15 +0800

    firstcommit

---
+++ src/main/java/cn/yearcon/yrcocrmapi/YrcocrmapiApplication.java
@@ -0,0 +1,12 @@
+package cn.yearcon.yrcocrmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YrcocrmapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YrcocrmapiApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/yearcon/yrcocrmapi/YrcocrmapiApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.yearcon.yrcocrmapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YrcocrmapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
